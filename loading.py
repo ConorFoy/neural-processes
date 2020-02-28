baa1e794f2c4572af8ce05817dbff46efc1043ec
Igor Adamski <igor.adamski2@gmail.com>
2020-02-28 00:07:46 +0000

    fix dimension -15 out of bounds

--- loading.py
+++ loading.py
@@ -272,7 +272,7 @@ class DataObject(DataLinks):
         
         examples_per_song = batch_size/songs_per_batch
 
-        target_split = random.randint(0, (self.test_tms)-11)
+        target_split = random.randint(0, (self.test_tms)-20)
         
         for idx, link in enumerate(random_songs):
             
